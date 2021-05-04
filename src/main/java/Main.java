a92de58f7c86147cbf56bbfcb25fb9b2ff09269c
Isac Ciobota <ciobotaisac@gmail.com>
2021-05-04 21:31:28 +0300

    SignIn & Register done

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import services.FileSystemService;
+import services.UserService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        initDirectory();
+        UserService.initDatabase();
+
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+        primaryStage.setTitle("Transport Application");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    private void initDirectory() {
+        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
+        if (!Files.exists(applicationHomePath))
+            applicationHomePath.toFile().mkdirs();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
