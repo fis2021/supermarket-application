d24c812b34861255d1024b1f58ba91ba6e831461
Alexandru Chifor <alexandru.chifor@student.upt.ro>
2021-05-14 21:22:34 +0300

    UserServiceTest modified and completed

--- src/main/java/services/UserServiceTest.java
+++ src/main/java/services/UserServiceTest.java
@@ -19,44 +19,46 @@ class UserServiceTest {
 
     @Test
     void TestBazaDeDateGoala(){
-        //assertThat(UserService.getAllUsers()).isNotNull();
-        //assertThat(UserService.getAllUsers()).isEmpty();
+        assertNotEquals(null,UserService.getAllUsers());  //assertThat(UserService.getAllUsers()).isNotNull();
+        assertTrue(UserService.getAllUsers().isEmpty());   //assertThat(UserService.getAllUsers()).isEmpty();
     }
 
     @Test
     void UserIsAdded() throws UsernameAlreadyExistsException {
         UserService.addUser("adminul","adminul","admin","adminul","adminul","adminul");
-        //assertThat(UserService.getAllUsers()).isNotEmpty();
-        //assertThat(UserService.getAllUsers()).size().isEqualTo(1);
+
+
+        assertNotEquals(null,UserService.getAllUsers());    //assertThat(UserService.getAllUsers()).isNotEmpty();
+        assertEquals(1,UserService.getAllUsers().size());   //assertThat(UserService.getAllUsers()).size().isEqualTo(1);
         User user=UserService.getAllUsers().get(0);
-        //assertThat(user).isNotNull();
-        /*assertThat(user.getUsername()).isEqualTo("adminul");
-        assertThat(user.getRole()).isEqualTo("adminul");
-        assertThat(user.getName()).isEqualTo("adminul");
-        assertThat(user.getAddress()).isEqualTo("adminul");
-        assertThat(user.getEmail()).isEqualTo("adminul");*/
+        assertNotEquals(null,UserService.getAllUsers());   //assertThat(user).isNotNull();
+        assertEquals("adminul",user.getUsername());         //assertThat(user.getUsername()).isEqualTo("adminul");
+        assertEquals("admin",user.getRole());               //assertThat(user.getRole()).isEqualTo("admin");
+        assertEquals("adminul",user.getName());     //assertThat(user.getName()).isEqualTo("adminul");
+        assertEquals("adminul",user.getAddress());     //assertThat(user.getAddress()).isEqualTo("adminul");
+        assertEquals("adminul",user.getEmail());     //assertThat(user.getEmail()).isEqualTo("adminul");
     }
 
     @Test
     void UserCannotBeAddedTwice() {
-        /*AssertThrows(UsernameAlreadyExistsException.class, () ->{
+        assertThrows(UsernameAlreadyExistsException.class, () ->{
          UserService.addUser("adminul","adminul","admin","adminul","adminul","adminul");
          UserService.addUser("adminul","adminul","admin","adminul","adminul","adminul");
-    });*/
+    });
     }
 
     @Test
     void UsernameAlreadyExists() {
-        /*AssertThrows(UsernameAlreadyExistsException.class, () ->{
+         assertThrows(UsernameAlreadyExistsException.class, () ->{
          UserService.addUser("adminul","adminul","admin","adminul","adminul","adminul");
          UserService.checkUserDoesNotAlreadyExist("adminul");
-    });*/
+    });
     }
 
     @Test
     void UserRoleIsCorrect() throws UsernameAlreadyExistsException {
          UserService.addUser("adminul","adminul","admin","adminul","adminul","adminul");
-         //assertThat(UserService.getUserRole("adminul", "adminul")).isEqualTo("admin");
+         assertEquals("admin", UserService.getUserRole("adminul", "adminul"));     //assertThat(UserService.getUserRole("adminul", "adminul")).isEqualTo("admin");
     }
 
 }
