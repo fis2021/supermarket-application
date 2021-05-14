758b02b2b8df14b950c0d12c5f15bbcda3552282
Alexandru Chifor <alexandru.chifor@student.upt.ro>
2021-05-14 19:25:33 +0300

    OrderServiceTest tests added

--- src/main/java/services/OrderServiceTest.java
+++ src/main/java/services/OrderServiceTest.java
@@ -1,5 +1,12 @@
 package services;
 
+import controllers.ClientController;
+import exceptions.CartIsEmptyException;
+import exceptions.NotEnoughQuantity;
+import exceptions.ProductAlreadyExistsException;
+import exceptions.ProductDoesNotExist;
+import model.Order;
+import model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +16,7 @@ class OrderServiceTest {
 
     @BeforeEach
     void setUp() throws Exception{
-        FileSystemService.APPLICATION_FOLDER=".testInregistrare";
+        FileSystemService.APPLICATION_FOLDER=".testOrder";
         //FileUtils.cleanDirectory(FileSystemService.getApplicationHomeFolder().toFile());
         OrderService.initDatabase();
     }
@@ -20,4 +27,27 @@ class OrderServiceTest {
         //assertThat(OrderService.getAllOrders()).isEmpty();
     }
 
+    @Test
+    void OrderAdded() throws ProductAlreadyExistsException, ProductDoesNotExist, NotEnoughQuantity, CartIsEmptyException {
+        Order order= new Order();
+        ProductService.addProduct("Paine", "Paine", "001", 10, 10);
+        order.addProduct(new Product("Paine", 5));
+        OrderService.placeOrder(order);
+        //assertThat(OrderService.getAllOrders().get(0)).isEqualTo(order);
+    }
+
+    @Test
+    void OrdersAreRemoved() throws CartIsEmptyException, ProductDoesNotExist, NotEnoughQuantity, ProductAlreadyExistsException {
+        Order order= new Order();
+        ProductService.addProduct("Paine", "Paine", "001", 10, 10);
+        order.addProduct(new Product("Paine", 5));
+        //assertThat(OrderService.getAllOrders()).isNotNull();
+        //assertThat(OrderService.getAllOrders()).isEmpty();
+        OrderService.placeOrder(order);
+        //assertThat(OrderService.getAllOrders()).isNotEmpty();
+        //assertThat(OrderService.getAllOrders()).size().isEqualTo(1);
+        OrderService.removeAllOrders();
+        //assertThat(OrderService.getAllOrders()).isNotNull();
+        //assertThat(OrderService.getAllOrders()).isEmpty();
+    }
 }
