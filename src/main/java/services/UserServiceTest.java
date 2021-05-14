b82c4780d48488409958beb09278cc10e87400b1
Alexandru Chifor <alexandru.chifor@student.upt.ro>
2021-05-14 13:50:23 +0300

    First test file

--- src/main/java/services/UserServiceTest.java
+++ src/main/java/services/UserServiceTest.java
@@ -11,13 +11,14 @@ class UserServiceTest {
     @BeforeEach
     void setUp(){
         FileSystemService.APPLICATION_FOLDER=".testInregistrare";
-        //FileUtils.
+        //FileUtils.cleanDirectory();
     }
 
     @Test
     void TestBazaDeDateGoala(){
         System.out.println("testul1");
-        assertThat(UserService.getAllUsers());
+       // assertThat(UserService.getAllUsers());
+
     }
 
 
