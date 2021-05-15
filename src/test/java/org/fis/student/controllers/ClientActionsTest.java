517e4201e794219d6ce1e29525b7c9bb7a02068a
Alexandru Chifor <alexandru.chifor@student.upt.ro>
2021-05-15 17:15:01 +0300

    ClientActionsTest basic tests

---
+++ src/test/java/org/fis/student/controllers/ClientActionsTest.java
@@ -0,0 +1,86 @@
+package org.fis.student.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
+import org.fis.student.exceptions.ProductAlreadyExistsException;
+import org.fis.student.services.FileSystemService;
+import org.fis.student.services.OrderService;
+import org.fis.student.services.ProductService;
+import org.fis.student.services.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.testfx.assertions.api.Assertions.assertThat;
+
+@ExtendWith(ApplicationExtension.class)
+
+class ClientActionsTest {
+
+    public static final String CODE = "001";
+    public static final String NAME = "Paine";
+    public static final String CATEGORY = "Paine";
+    public static final String QUANTITY = "5";
+    public static final String PRICE = "5";
+
+    @BeforeEach
+    void setUp() throws Exception{
+        FileSystemService.APPLICATION_FOLDER=".testRegister";
+        FileSystemService.initDirectory();
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomeFolder().toFile());
+        UserService.initDatabase();
+        OrderService.initDatabase();
+        ProductService.initDatabase();
+    }
+
+    @AfterEach
+    void tearDown() {
+        OrderService.getDatabase().close();
+        ProductService.getDatabase().close();
+        UserService.getDatabase().close();
+    }
+
+    @Start
+    void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("client.fxml"));
+        primaryStage.setTitle("User Actions Example");
+        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.show();
+    }
+
+    @Test
+    void TestViewProducts(FxRobot robot) throws ProductAlreadyExistsException {
+        robot.clickOn("#ClientViewProducts");
+        robot.clickOn("#backViewProducts");
+
+        robot.clickOn("#clientAddToCart");
+        assertThat(robot.lookup("#clientMessage").queryText()).hasText("Please type in a name!");
+        robot.clickOn("#productNameField");
+        robot.write(NAME);
+
+        robot.clickOn("#clientAddToCart");
+        assertThat(robot.lookup("#clientMessage").queryText()).hasText("Please type in the quantity!");
+        robot.clickOn("#quantityField");
+        robot.write("abc");
+
+        robot.clickOn("#clientAddToCart");
+        assertThat(robot.lookup("#clientMessage").queryText()).hasText("Quantity must be an Integer!");
+        robot.clickOn("#quantityField");
+        robot.eraseText(3);
+        robot.write(QUANTITY);
+
+        robot.clickOn("#clientAddToCart");
+        assertThat(robot.lookup("#clientMessage").queryText()).hasText("The product Paine doesn't exists!");
+        ProductService.addProduct(NAME, CATEGORY, CODE, Integer.parseInt(QUANTITY), Integer.parseInt(PRICE));
+        robot.clickOn("#clientAddToCart");
+        robot.clickOn("#viewCart");
+    }
+}
